0656b59042afe5adc8c478584710c1b586390e1d
Fladiem <Fladm@live.com>
2026-06-04 21:51:55 -0400

    B: Added basic user input scanning logic for CLI

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	userInput := bufio.NewScanner(os.Stdin)
+
+	for ; ; {
+		//default cli message
+		fmt.Print("Pokedex > ")
+		//block until user presses enter
+		userInput.Scan()
+		//convert input to string
+		uString := userInput.Text()
+		//convert string to list of strings, strip whitespace, lowercase conversion
+		textCln := cleanInput(uString)
+		//print first word userInput, very useful and productive
+		fmt.Println("Your command was:", textCln[0])
+	}
 
 	return
 }
